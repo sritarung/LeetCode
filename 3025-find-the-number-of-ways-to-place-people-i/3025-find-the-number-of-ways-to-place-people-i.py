5afeeb669dc5f466395408631a074f00a3e66af1
Sri Tarun Gulumuru <sritarung96@gmail.com>
2025-09-02 22:43:11 -0400

    Time: 254 ms (14.29%), Space: 17.7 MB (57.14%) - LeetHub

--- 3025-find-the-number-of-ways-to-place-people-i/3025-find-the-number-of-ways-to-place-people-i.py
+++ 3025-find-the-number-of-ways-to-place-people-i/3025-find-the-number-of-ways-to-place-people-i.py
@@ -1,35 +1,40 @@
 class Solution:
     def numberOfPairs(self, points: List[List[int]]) -> int:
+        
+        # given a list of points
+        # count no. of pairs of points that meet
+            # A point is on upper left side of B
+            # no points in the line or rectangle they make
+        
+
+         # naive n^3 approach
         ans = 0
         n = len(points)
 
         for i in range(n):
-            pointA = points[i]
+            A = points[i]
             for j in range(n):
-                pointB = points[j]
-                if i == j or not (
-                    pointA[0] <= pointB[0] and pointA[1] >= pointB[1]
-                ):
+                B = points[j]
+                if i == j or not (A[0] <= B[0] and A[1]>= B[1]):
                     continue
+                
                 if n == 2:
                     ans += 1
                     continue
-
                 illegal = False
                 for k in range(n):
                     if k == i or k == j:
                         continue
+                    
+                    temp = points[k]
+                    isX = (temp[0]>= A[0] and temp[0]<= B[0])
+                    isY = (temp[1]<=A[1] and temp[1]>=B[1])
 
-                    pointTmp = points[k]
-                    isXContained = (
-                        pointTmp[0] >= pointA[0] and pointTmp[0] <= pointB[0]
-                    )
-                    isYContained = (
-                        pointTmp[1] <= pointA[1] and pointTmp[1] >= pointB[1]
-                    )
-                    if isXContained and isYContained:
+                    if isX and isY:
                         illegal = True
                         break
                 if not illegal:
                     ans += 1
         return ans
+
+
