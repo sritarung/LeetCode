bca7247832aae8d719be6d1f9b2405ff1cbc76b2
Sri Tarun Gulumuru <sritarung96@gmail.com>
2025-09-11 14:53:57 -0400

    Time: 0 ms (100%), Space: 17.9 MB (40.37%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,13 +1,26 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        # cascading
-        output = [[]]
-        for num in nums:
-            newSubsets = []
-            for curr in output:
-                temp = curr.copy()
-                temp.append(num)
-                newSubsets.append(temp)
-            for curr in newSubsets:
-                output.append(curr)
+        # backtrack
+        output = []
+        curr = []
+        def backtrack(index):
+            output.append(curr[:])
+            for i in range(index, len(nums)):
+                curr.append(nums[i])
+                backtrack(i+1)
+                curr.pop()
+        backtrack(0)
         return output
+
+
+        # cascading
+        # output = [[]]
+        # for num in nums:
+        #     newSubsets = []
+        #     for curr in output:
+        #         temp = curr.copy()
+        #         temp.append(num)
+        #         newSubsets.append(temp)
+        #     for curr in newSubsets:
+        #         output.append(curr)
+        # return output
