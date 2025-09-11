548d5a2ce33bc370f315c8e788db2c41d1076f42
Sri Tarun Gulumuru <sritarung96@gmail.com>
2025-09-11 12:24:34 -0400

    Time: 1781 ms (17.61%), Space: 18.4 MB (86.78%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {}
+        # brute force solution
+
+        # iterate through the entire nums list in a double for loop
+        # for each element, check if there is a complement element in the rest of the list
         for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in hashmap:
-                return [i, hashmap[complement]]
-            hashmap[nums[i]] = i
-        # Return an empty list if no solution is found
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
         return []
+        # time complexity is O(n^2) 
