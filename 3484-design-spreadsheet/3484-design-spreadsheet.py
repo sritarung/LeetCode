f6f12a11e7e565b8d2539c04a886375f4efa24a5
Sri Tarun Gulumuru <sritarung96@gmail.com>
2025-09-18 22:45:57 -0400

    Time: 73 ms (89.02%), Space: 23.7 MB (49.77%) - LeetHub

--- 3484-design-spreadsheet/3484-design-spreadsheet.py
+++ 3484-design-spreadsheet/3484-design-spreadsheet.py
@@ -1,36 +1,25 @@
 class Spreadsheet:
 
     def __init__(self, rows: int):
-        self.matrix = [[0]*26 for _ in range(rows)]
+        self.mp = {}
+
 
     def setCell(self, cell: str, value: int) -> None:
-        col = ord(cell[0]) - 65
-        row = int(cell[1:]) - 1
-        self.matrix[row][col] = value  
+        self.mp[cell] = value
 
     def resetCell(self, cell: str) -> None:
-        col = ord(cell[0]) - 65
-        row = int(cell[1:]) - 1
-        self.matrix[row][col] = 0
+        self.mp.pop(cell,None)
 
     def getValue(self, formula: str) -> int:
-        expression = formula[1:]
-        x, y = expression.split("+", 1)
-        if x.isdigit() and y.isdigit():
-            return int(x) + int(y)
-        else:
-            a = None
-            b = None
-            if not x.isdigit():
-                row1, col1 = int(x[1:]) - 1, ord(x[0]) - 65
-            else:
-                a = int(x)
-            if not y.isdigit():
-                row2, col2 = int(y[1:]) - 1, ord(y[0]) - 65
-            else:
-                b = int(y)
-            if a is None:
-                a = self.matrix[row1][col1]
-            if b is None:
-                b = self.matrix[row2][col2]
-            return a + b
+        x, y = formula[1:].split("+", 1)
+        return self.token_val(x) + self.token_val(y)
+
+    def token_val(self, token):
+        return int(token) if token.isdigit() else self.mp.get(token, 0)
+
+
+# Your Spreadsheet object will be instantiated and called as such:
+# obj = Spreadsheet(rows)
+# obj.setCell(cell,value)
+# obj.resetCell(cell)
+# param_3 = obj.getValue(formula)
