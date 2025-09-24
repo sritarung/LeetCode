bacb462a7955c4093c649fa15cc7a89af5c3b159
Sri Tarun Gulumuru <sritarung96@gmail.com>
2025-09-24 19:24:31 -0400

    Time: 0 ms (100%), Space: 18.1 MB (20.36%) - LeetHub

--- 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.py
+++ 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.py
@@ -4,32 +4,28 @@ class Solution:
             return "0"
 
         res = []
-
         if (numerator < 0) ^ (denominator < 0):
             res.append("-")
-
-        
         numerator = abs(numerator)
         denominator = abs(denominator)
-
         res.append(str(numerator//denominator))
         remainder = numerator % denominator
+
         if remainder == 0:
             return "".join(res)
-
+        
         res.append(".")
         remainder_map = {}
-
         while remainder:
             if remainder in remainder_map:
                 idx = remainder_map[remainder]
                 res.insert(idx, "(")
                 res.append(")")
                 break
-
             remainder_map[remainder] = len(res)
             remainder *= 10
             res.append(str(remainder//denominator))
             remainder %= denominator
         return "".join(res)
-        
+
+            
