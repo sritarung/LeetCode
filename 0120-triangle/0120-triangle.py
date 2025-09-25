f9e7dc5ca92884e21c201c93fb4e36589ddf2269
Sri Tarun Gulumuru <sritarung96@gmail.com>
2025-09-25 00:16:02 -0400

    Time: 19 ms (5.36%), Space: 21.4 MB (8.65%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,11 +1,17 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        for row in range(1, len(triangle)):
-            for col in range(row + 1):
-                smallest_above = math.inf
-                if col > 0:
-                    smallest_above = triangle[row - 1][col - 1]
-                if col < row:
-                    smallest_above = min(smallest_above, triangle[row - 1][col])
-                triangle[row][col] += smallest_above
-        return min(triangle[-1])
+        memo = {}
+        def backtrack(i, j):
+            if i == len(triangle)-1:
+                return triangle[i][j]
+            
+            if (i,j) in memo:
+                return memo[(i,j)]
+
+            down = backtrack(i+1, j)
+            diag = backtrack(i+1, j+1)
+            memo[(i,j)] = min(down, diag) + triangle[i][j]
+            return memo[(i,j)]
+        return backtrack(0,0)
+
+    
