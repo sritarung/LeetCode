b347a60407a54797ddd21562a80fccbbb9c977ee
Sri Tarun Gulumuru <sritarung96@gmail.com>
2025-09-28 04:28:20 -0400

    Time: 418 ms (84.25%), Space: 17.8 MB (53.27%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.py
+++ 0611-valid-triangle-number/0611-valid-triangle-number.py
@@ -1,15 +1,16 @@
 class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
+        #For a list of three numbers to form a triangle, the Triangle Inequality Theorem states that the sum of the lengths of any two sides must be strictly greater than the length of the third side
+
         nums.sort()
         count = 0
-        for k in range(len(nums) - 1, -1, -1):
-            i = 0
-            j = k - 1
-            while i < j:
-                if nums[i] + nums[j] > nums[k]:
-                    count += j - i
-                    j -= 1
+        for i in range(len(nums)-1, -1, -1):
+            j = 0
+            k = i - 1
+            while j < k:
+                if nums[j] + nums[k] > nums[i]:
+                    count += k - j
+                    k -= 1
                 else:
-                    i += 1
+                    j += 1
         return count
-
