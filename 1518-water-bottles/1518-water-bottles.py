a93c31adc089720591fa892c21705b53e762b3ae
Sri Tarun Gulumuru <sritarung96@gmail.com>
2025-10-01 18:58:48 -0400

    Time: 0 ms (100%), Space: 18 MB (17.14%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.py
+++ 1518-water-bottles/1518-water-bottles.py
@@ -1,10 +1,13 @@
 class Solution:
     def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
-        total = numBottles
-        empty = 0
-        while numBottles >= numExchange:
-            numBottles //= numExchange
-            empty = numBottles % numExchange
-            total += numBottles
-            numBottles += empty
+        total = numBottles  # all initial bottles get drunk
+        empty = numBottles  # now they’re empty
+        
+        while empty >= numExchange:
+            # exchange empties for new bottles
+            new_bottles = empty // numExchange
+            total += new_bottles  # drink them
+            # update empties = leftover empties + new empties
+            empty = empty % numExchange + new_bottles
+        
         return total
