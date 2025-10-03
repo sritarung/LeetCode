f7707a9e39fb1ca20683b6e15b7e6e68c2f98926
Sri Tarun Gulumuru <sritarung96@gmail.com>
2025-10-03 15:00:48 -0400

    Time: 139 ms (40.1%), Space: 19.6 MB (98.68%) - LeetHub

--- 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.py
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.py
@@ -2,32 +2,28 @@ class Solution:
     def trapRainWater(self, heightMap: List[List[int]]) -> int:
         if not heightMap or not heightMap[0]:
             return 0
-        
-        m = len(heightMap)
-        n = len(heightMap[0])
+        row = len(heightMap)
+        col = len(heightMap[0])
 
-        visited = [[False]* n for _ in range(m)]
-        heap = []
+        minHeap = []
+        for i in range(row):
+            for j in range(col):
+                if i == 0 or i == row - 1 or j == 0 or j == col - 1:
+                    heapq.heappush(minHeap, (heightMap[i][j], i, j))
+                    heightMap[i][j] = -1
 
-        for i in range(m):
-            for j in range(n):
-                if i == 0 or j == 0 or i == m -1 or j == n -1:
-                    heapq.heappush(heap, (heightMap[i][j], i, j))
-                    visited[i][j] = True
-        
         res = 0
+        directions = [(0,1), (1, 0), (0, -1), (-1, 0)]
         maxHeight = 0
-        directions = [(0,1), (1, 0), (-1, 0), (0, -1)]
-        while heap:
-            height, x, y = heapq.heappop(heap)
-            maxHeight = max(height, maxHeight)
+        while minHeap:
+            height, x, y = heapq.heappop(minHeap)
+            maxHeight = max(maxHeight, height)
 
             for dx, dy in directions:
                 nx, ny = x + dx, y + dy
-                if 0 <= nx < m and 0 <= ny < n and not visited[nx][ny]:
-                    res += max(0, maxHeight - heightMap[nx][ny])
-                    visited[nx][ny] = True
-                    heapq.heappush(heap, (max(heightMap[x][y], heightMap[nx][ny]),nx, ny))
+
+                if 0 <= nx < row and 0 <= ny < col and heightMap[nx][ny] != -1:
+                    res +=  max(0,maxHeight - heightMap[nx][ny])
+                    heapq.heappush(minHeap, (max(height, heightMap[nx][ny]), nx, ny))
+                    heightMap[nx][ny] = -1
         return res
-
-
