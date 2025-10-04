361c4450dcc35b6479389f3c8a47431f2adc1501
Sri Tarun Gulumuru <sritarung96@gmail.com>
2025-10-03 20:19:08 -0400

    Time: 4 ms (89.15%), Space: 18.5 MB (92.14%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,20 +1,13 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        memo = {}
-        def backtrack(i, steps):
-            if i >= len(nums)-1:
-                return 0
-            if i in memo:
-                return memo[i]
-
-            minSteps = float('inf')
-            
-            for j in range(i+1, min(len(nums),i+nums[i]+1)):
-                jumps_next = backtrack(j, steps+1)
-                if jumps_next != float('inf'):
-                    minSteps = min(minSteps, jumps_next + 1)
-            memo[i] = minSteps
-            return minSteps
-        return backtrack(0,0)
-
+        jumps = 0
+        l = r = 0
+        while r < len(nums)-1:
+            farthest = 0
+            for i in range(l, r+1):
+                farthest = max(farthest, nums[i]+i)
+            l = r + 1
+            r = farthest
+            jumps += 1
+        return jumps
 
