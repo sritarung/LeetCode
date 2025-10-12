21bfd3180351b1e60befb7f0adb17b625a2f1bae
Sri Tarun Gulumuru <sritarung96@gmail.com>
2025-10-11 22:08:51 -0400

    Time: 427 ms (80.33%), Space: 46.7 MB (42.89%) - LeetHub

--- 3186-maximum-total-damage-with-spell-casting/3186-maximum-total-damage-with-spell-casting.py
+++ 3186-maximum-total-damage-with-spell-casting/3186-maximum-total-damage-with-spell-casting.py
@@ -1,16 +1,26 @@
 class Solution:
     def maximumTotalDamage(self, power: List[int]) -> int:
-        count = Counter(power)
-        vec = [(-(10**9), 0)]
-        for k in sorted(count.keys()):
-            vec.append((k, count[k]))
-        n = len(vec)
-        f = [0] * n
-        mx = 0
-        j = 1
-        for i in range(1, n):
-            while j < i and vec[j][0] < vec[i][0] - 2:
-                mx = max(mx, f[j])
+        freq = Counter(power)
+        vals = sorted(freq.keys())
+        n = len(vals)
+        score = [v * freq[v] for v in vals]
+
+        # 2) For each i, find the next index j with vals[j] >= vals[i] + 3
+        #    (since choosing vals[i] forbids i±1 and i±2)
+        next_idx = [n] * n
+        j = 0
+        for i in range(n):
+            if j < i:
+                j = i
+            while j < n and vals[j] < vals[i] + 3:
                 j += 1
-            f[i] = mx + vec[i][0] * vec[i][1]
-        return max(f)
+            next_idx[i] = j
+
+        # 3) DP from the back: dp[i] = max total starting at i
+        dp = [0] * (n + 1)
+        for i in range(n - 1, -1, -1):
+            take = score[i] + dp[next_idx[i]]
+            skip = dp[i + 1]
+            dp[i] = max(take, skip)
+
+        return dp[0] 
