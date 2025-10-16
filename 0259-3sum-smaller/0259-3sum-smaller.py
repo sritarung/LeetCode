01fb479006a3a06f16ac07f33bfe88d3f897746d
Sri Tarun Gulumuru <sritarung96@gmail.com>
2025-10-16 14:58:09 -0400

    Time: 1996 ms (8.93%), Space: 17.8 MB (86.75%) - LeetHub

---
+++ 0259-3sum-smaller/0259-3sum-smaller.py
@@ -0,0 +1,11 @@
+class Solution:
+    def threeSumSmaller(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        ans = 0
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                complement = target - nums[i] - nums[j]
+                left = bisect_left(nums, complement, j+1)
+                ans += left - j-1
+        return ans 
+                
