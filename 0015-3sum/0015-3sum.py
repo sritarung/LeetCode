7e0ad8ba96c5f3c4fb7ca6ac1375553df3e858f7
Sri Tarun Gulumuru <sritarung96@gmail.com>
2025-11-18 18:24:48 -0500

    Time: 461 ms (84.94%), Space: 20.8 MB (20.62%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,39 +1,57 @@
+
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # since we are returning the elements and not indicides, we can use sorting
-        # sort nums
-        # nums = [-1,0,1,2,-1,-4]
-        nums.sort()
-        # nums = [-4, -1, -1, 0, 1, 2]
+    def twoSum(self, nums, target, start):
+        results = []  # FIX 1: Create a list to hold ALL pairs
+        left = start
+        right = len(nums) - 1
         
+        while left < right:
+            current_sum = nums[left] + nums[right]
+            
+            if current_sum < target:
+                left += 1
+            elif current_sum > target:
+                right -= 1
+            else:
+                # FIX 2: Found a match! Add VALUES, not indices.
+                results.append([nums[left], nums[right]])
+                
+                # FIX 3: Skip duplicates internally to avoid repeating pairs
+                left += 1
+                right -= 1
+                while left < right and nums[left] == nums[left - 1]:
+                    left += 1
+                while left < right and nums[right] == nums[right + 1]:
+                    right -= 1
+                    
+        return results # Return list of lists
+            
+    def kSumHelper(self, nums, target, start, k):
+        results = []
         
-        # nums = [-4, -4,-4, 0, 0, 1, 3, 4, 4]
-        # Notice that the solution set must not contain duplicate triplets
-        # -4 - 4 + 4 = -4
-        # -4 + 0 + 4 = 0
-        #[-1,-1,-1,0,2]
-        output = []
-        for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i - 1]:
+        # Base Case
+        if k == 2:
+            return self.twoSum(nums, target, start)
+        
+        # Recursive Step
+        for i in range(start, len(nums) - k + 1):
+            # Skip duplicates for the current position
+            if i > start and nums[i-1] == nums[i]:
                 continue
-            l = i + 1
-            r = len(nums) - 1
-            while l < r:
-                sum_numbers = nums[i] + nums[l] + nums[r]
-                if sum_numbers == 0:
-                    output.append([nums[i],nums[l],nums[r]])
-                    while l < r and nums[l]==nums[l+1]:
-                        l += 1
-                    l += 1
-                    while l < r and nums[r] == nums[r-1]:
-                        r -= 1
-                    r -= 1
-                elif sum_numbers < 0:
-                    while l < r and nums[l]==nums[l+1]:
-                        l += 1
-                    l += 1
-                else:
-                    while l < r and nums[r] == nums[r-1]:
-                        r -= 1
-                    r -= 1
-        return output
+            
+            current_num = nums[i]
+
+            # Recursively find (k-1) sum
+            sub_result = self.kSumHelper(nums, target - current_num, i + 1, k - 1)
+
+            # Combine current number with the results from recursion
+            for sub_list in sub_result:
+                results.append([current_num] + sub_list)
+                
+        return results
+
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        # k=3, target=0
+        return self.kSumHelper(nums, 0, 0, 3)
+       
