71ebeafe07d92003411a89b2eb388f1f5a025259
Sri Tarun Gulumuru <sritarung96@gmail.com>
2025-11-18 18:47:02 -0500

    Time: 859 ms (20.53%), Space: 20.2 MB (97.03%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,57 +1,29 @@
-
 class Solution:
-    def twoSum(self, nums, target, start):
-        results = []  # FIX 1: Create a list to hold ALL pairs
-        left = start
-        right = len(nums) - 1
-        
-        while left < right:
-            current_sum = nums[left] + nums[right]
-            
-            if current_sum < target:
-                left += 1
-            elif current_sum > target:
-                right -= 1
-            else:
-                # FIX 2: Found a match! Add VALUES, not indices.
-                results.append([nums[left], nums[right]])
-                
-                # FIX 3: Skip duplicates internally to avoid repeating pairs
-                left += 1
-                right -= 1
-                while left < right and nums[left] == nums[left - 1]:
-                    left += 1
-                while left < right and nums[right] == nums[right + 1]:
-                    right -= 1
-                    
-        return results # Return list of lists
-            
-    def kSumHelper(self, nums, target, start, k):
-        results = []
-        
-        # Base Case
-        if k == 2:
-            return self.twoSum(nums, target, start)
-        
-        # Recursive Step
-        for i in range(start, len(nums) - k + 1):
-            # Skip duplicates for the current position
-            if i > start and nums[i-1] == nums[i]:
-                continue
-            
-            current_num = nums[i]
-
-            # Recursively find (k-1) sum
-            sub_result = self.kSumHelper(nums, target - current_num, i + 1, k - 1)
-
-            # Combine current number with the results from recursion
-            for sub_list in sub_result:
-                results.append([current_num] + sub_list)
-                
-        return results
-
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = []
         nums.sort()
-        # k=3, target=0
-        return self.kSumHelper(nums, 0, 0, 3)
-       
+        for i in range(len(nums)-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            l = i+1
+            r = len(nums)-1
+            while l < r:
+                total = nums[i]+ nums[l] + nums[r]
+                if total > 0:
+                    while l < r and nums[r] == nums[r-1]:
+                        r -= 1
+                    r -= 1
+                elif total < 0:
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
+                    l += 1
+                else:
+                    ans.append((nums[i], nums[l], nums[r]))
+                    while l < r and nums[r] == nums[r-1]:
+                        r -= 1
+                    r -= 1
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
+                    l += 1
+        return ans
+
