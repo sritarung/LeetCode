3e87d556a9a489c353fab777e9e1d904756ea396
Sri Tarun Gulumuru <sritarung96@gmail.com>
2025-11-20 14:10:34 -0500

    Time: 1798 ms (5.01%), Space: 18 MB (30.21%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -1,19 +1,26 @@
 class Solution:
+    def bisectRight(self, nums, target, idx):
+        lo = idx
+        hi = len(nums)
+        while lo < hi:
+            mid = lo + (hi - lo)//2
+            if nums[mid] > target:
+                hi = mid 
+            else:
+                lo = mid + 1
+        return lo
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         diff = float('inf')
         nums.sort()
         for i in range(len(nums)):
-            lo = i + 1
-            hi = len(nums)-1
-            while lo < hi:
-                total = nums[lo] + nums[hi] + nums[i]
-                if abs(target - total) < abs(diff):
-                    diff = target - total
-                if total < target:
-                    lo += 1
-                else:
-                    hi -= 1
+            for j in range(i+1, len(nums)):
+                complement = target - nums[i] - nums[j]
+                hi = self.bisectRight(nums, complement, j+1)
+                lo = hi -1
+                if hi < len(nums) and abs(diff) > abs(complement - nums[hi]):
+                    diff = complement - nums[hi]
+                if lo > j and abs(diff) > abs(complement - nums[lo]):
+                    diff = complement - nums[lo]
             if diff == 0:
                 break
         return target - diff
-
