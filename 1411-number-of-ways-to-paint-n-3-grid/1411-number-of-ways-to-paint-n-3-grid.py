34301784bb6eb433112fd4e3a53e3b25a42411ee
Sri Tarun Gulumuru <sritarung96@gmail.com>
2025-11-20 15:52:35 -0500

    Time: 7 ms (84.19%), Space: 17.7 MB (93.59%) - LeetHub

--- 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.py
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.py
@@ -1,10 +1,20 @@
 class Solution:
     def numOfWays(self, n: int) -> int:
-        MOD = (10 ** 9) + 7
-        unique=6
-        repeating=6
-        for i in range(2,n+1):
-            temp=unique
-            unique=3*unique+2*repeating
-            repeating=2*temp+2*repeating
-        return (unique+repeating)%MOD
+        MOD = 10**9 + 7
+        
+        # Base Case for N=1
+        # 6 ways to do ABC (3 colors)
+        # 6 ways to do ABA (2 colors)
+        abc = 6
+        aba = 6
+        
+        for i in range(2, n + 1):
+            # Calculate new values based on the formula
+            new_abc = (2 * abc + 2 * aba) % MOD
+            new_aba = (2 * abc + 3 * aba) % MOD
+            
+            # Update states
+            abc = new_abc
+            aba = new_aba
+            
+        return (abc + aba) % MOD
