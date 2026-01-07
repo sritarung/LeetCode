982e7dbe74411ae27adcd01832d17672a5643609
Sri Tarun Gulumuru <sritarung96@gmail.com>
2026-01-07 18:26:49 -0500

    Time: 67 ms (70.09%), Space: 19.8 MB (5.2%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,25 +1,35 @@
-from collections import Counter, defaultdict
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        need = Counter(t)
-        have = defaultdict(int)
-        required = len(need)
-        formed = 0
-        min_len = float('inf')
-        start = 0
-        l = 0
-        for r in range(len(s)):
-            have[s[r]] += 1
-            if s[r] in need and have[s[r]] == need[s[r]]:
-                formed += 1
+        # this is like permutation in string, but return minimum string not true or false
+        # need to return the minimum window substring of s that contains all the characters in string t. if no, return ""
+        # input contains upper and lowercase english chars(52)
+        # both the strings are of at least length one another edge case
+        # if of same length, check counter of each char in both match or else return "" - same logic
+        counter_t = Counter(t)
+        need = len(counter_t)
+        have = 0
+        counter_s = defaultdict(int)
+        left = 0
+        idx = 0
+        ans = float('inf')
+        for right in range(len(s)):
+            char = s[right]
+            if char in counter_t:
+                counter_s[char] += 1
+                if counter_s[char] == counter_t[char]:
+                    have += 1
                 
-            while formed == required:
-                if r - l + 1 < min_len:
-                    min_len = r - l + 1
-                    start = l
+            while have == need and left <=right:
+                if right - left + 1 < ans:
+                    ans = right - left + 1
+                    idx = left
+                left_char = s[left]
+                counter_s[left_char] -= 1
+                if left_char in counter_t:
+                    if counter_s[left_char] == counter_t[left_char] - 1:
+                        have -= 1
+                left += 1
+        return "" if ans == float('inf') else s[idx: idx + ans]
 
-                have[s[l]] -= 1
-                if s[l] in need and have[s[l]] < need[s[l]]:
-                    formed -= 1
-                l += 1
-        return "" if min_len == float('inf') else s[start:start+min_len]
+
+        
